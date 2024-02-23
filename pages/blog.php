3d6702ead8bbbf3ccdbb3aadcd401100e79ba06c
Danilo Alves <danilo.alves@f2f-digital.com>
2024-02-22 22:36:52 -0300

    atualização de blog

--- pages/blog.php
+++ pages/blog.php
@@ -6,10 +6,44 @@
 
 get_header();
 
+$title = get_field('titulo');
+$text_intro = get_field('text_intro');
+$posts = get_field('posts_de_blog');
+$img_blog = get_field('img_blog');
+$title_post = get_field('title_post');
+$text_post = get_field('text_post');
+
 ?>
 
+<main class="container-fluid">
+
+    <div class="blog__intro">
+        <div class="column-heading col-5">
+            <h2 class="blog__intro-heading"><?php echo ($title); ?></h2>
+        </div>
+        <div class="blog__intro-description col-7">
+            <p><?php echo ($text_intro); ?> </p>
+        </div>
+    </div>
 
 
+    <?php foreach ($posts as $post) { ?>
+        <article class="post">
+
+            <div style="background-image: url('<?php echo $post['img_blog']; ?>')" class="img-post"></div>
+            <div class="content-post">
+                <h4><?php echo $post['title_post']; ?></h4>
+                <p>
+                    <?php echo $post['text_post']; ?>
+                </p>
+                <a class="link-leiamais" href="">
+                    <?php echo ('leia mais'); ?>
+                </a>
+            </div>
+        </article>
+    <?php } ?>
+
+</main>
 <?php
 get_footer();
 ?>
--- pages/content-blog.php
+++ pages/content-blog.php
@@ -6,4 +6,16 @@
 
 get_header();
 
+$hero_blog = get_field('hero_blog');
+$titulo_blog = get_field('titulo_blog');
+$conteudo_blog = get_field('conteudo_blog');
+
 ?>
+
+<div class="container">
+    <article class="content-blog">
+        <div style="background-image: url('<?php echo ($hero_blog); ?>') " class="hero"></div>
+        <h2><?php echo ($titulo_blog); ?></h2>
+        <p><?php echo ($conteudo_blog); ?> </p>
+    </article>
+</div>
