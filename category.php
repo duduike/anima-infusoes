2b4933a3b0d40495615094331fd22e4400349842
alefdav <alefdav@gmail.com>
2024-02-23 00:26:28 -0300

    blog e single

--- category.php
+++ category.php
@@ -1,4 +1,52 @@
 <?php get_header(); ?>
 
+<div class="container">
+    <div class="row">
+        <main class="col-12">
+            <?php if (have_posts()) : ?>
+
+                <?php
+                // Start the Loop.
+                while (have_posts()) : the_post(); ?>
+                    <article id="post-<?php get_the_ID(); ?>" <?php post_class(); ?>>
+
+                        <div class="post-body">
+                            <div style="background-image: url('<?php echo wp_get_attachment_url(get_post_thumbnail_id($post->ID), 'thumbnail'); ?>');" class="img-post"></div>
+                            <div class="content-post">
+                                <h4><?php echo get_the_title(); ?></h4>
+                                <div class="textContent">
+                                    <p><?php echo get_field('descricao', get_the_ID()) ?></p>
+                                </div>
+
+                                <a class="link-leiamais" href="<?php echo esc_url(get_permalink()) ?>">
+                                    leia mais
+                                </a>
+                            </div>
+                        </div>
+
+                    </article>
+
+
+                <?php endwhile; ?>
+
+            <?php
+                // Previous/next page navigation.
+                the_posts_pagination(array(
+                    'prev_text'          => __('Anterior'),
+                    'next_text'          => __('Próximo'),
+                    'screen_reader_text' => '&nbsp;'
+
+                ));
+
+            // If no content, include the "No posts found" template.
+            else :
+                get_template_part('content', 'none');
+
+            endif;
+            ?>
+
+        </main>
+    </div>
+</div>
 
 <?php get_footer(); ?>
--- single.php
+++ single.php
@@ -1,59 +1,79 @@
-<?php get_header(); ?>
+<?php
 
-<main class="container">
-    <div class="blog__intro">
-        <div class="column-heading">
-            <h2 class="blog__intro-heading">Lorem ipsum dolor sit amet, consectetur adipisicing elit. </h2>
-        </div>
-        <div class="blog__intro-description">
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta, natus ipsam ex quidem illum suscipit fuga beatae distinctio in assumenda saepe vel quam, blanditiis officia, corporis exercitationem veritatis sint eos!.</p>
-            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ducimus velit adipisci in. Error enim esse autem perspiciatis animi hic, fuga reprehenderit non id, temporibus dicta eos culpa incidunt possimus labore?</p>
-        </div>
+$args = array(
+    'post_type' => 'post',
+    'orderby'    => 'Date',
+    'post_status' => 'publish',
+    'order'    => 'DESC',
+    'category_name' => 'Blog',
+    'post__not_in' => array($post->ID),
+    'posts_per_page' => -1 // this will retrive all the post that is published 
+);
+$result = new WP_Query($args);
+
+get_header();
+?>
+
+<div class="container mt-5">
+    <div class="row">
+        <main class="col-12 d-flex flex-column flex-lg-row main-single">
+            <div class="col-lg-9 col-12">
+                <?php
+                // Start the Loop.
+                while (have_posts()) : the_post();
+                ?>
+                    <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+                        <div class="d-flex flex-column align-items-center">
+
+                            <header class="entry-header thumb-single">
+                                <img title="image title" alt="thumb image" class="wp-post-image" src="<?= wp_get_attachment_url(get_post_thumbnail_id()); ?>" style="width:100%; height:auto;">
+                            </header><!-- .entry-header -->
+
+                            <div class="entry-content">
+                                <h4 class="text-center mt-3 mb-3"><?php echo get_the_title(); ?></h4>
+                                </span>
+                                <div class="conteudoSingle">
+                                    <?php echo get_field('conteudo', get_the_ID()) ?>
+                                </div>
+                            </div><!-- .entry-content -->
+                        </div>
+
+
+                    </article><!-- #post-## -->
+
+
+                <?php
+
+                // End the loop.
+                endwhile;
+
+                ?>
+            </div>
+            <div class="col-lg-3 col-12">
+                <h4 class="text-uppercase">Leia também:</h4>
+
+                <div class="column-leiaMais d-flex flex-column">
+                    <?php if ($result->have_posts()) : ?>
+                        <?php while ($result->have_posts()) : $result->the_post(); ?>
+                            <a href="<?php echo esc_url(get_permalink()) ?>">
+                                <div class="bodyLeiaMais d-flex flex-column">
+                                    <img title="image title" alt="thumb image" class="wp-post-image" src="<?= wp_get_attachment_url(get_post_thumbnail_id()); ?>" style="width:100%; height:auto;">
+                                    <span class="title"><?php the_title(); ?></span>
+                                </div>
+                            </a>
+
+                        <?php endwhile; ?>
+                    <?php endif;
+                    wp_reset_postdata(); ?>
+                </div>
+            </div>
+
+
+
+        </main>
     </div>
-    <article>
-        <div class="post">
-            <div style="background-image: url('<?php echo get_template_directory_uri(); ?>/assets/images/1707148575-64603465-754x672--CH-0795-Pano-copiar.webp');" class="img-post"></div>
-            <div class="content-post">
-                <h4>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h4>
-                <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Suscipit nemo error eaque non, amet voluptate aut incidunt doloremque praesentium.
-                </p>
-                <a class="link-leiamais" href="">
-                    leia mais
-                </a>
-            </div>
-        </div>
-        <div class="post">
-            <div style="background-image: url('<?php echo get_template_directory_uri(); ?>/assets/images/1707148575-64603465-754x672--CH-0795-Pano-copiar.webp');" class="img-post"></div>
-            <div class="content-post">
-                <h4>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h4>
-                <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Suscipit nemo error eaque non, amet voluptate aut incidunt doloremque praesentium.
-                </p>
-                <a class="link-leiamais" href="">
-                    leia mais
-                </a>
-            </div>
-        </div>
-        <div class="post">
-            <div style="background-image: url('<?php echo get_template_directory_uri(); ?>/assets/images/1707148575-64603465-754x672--CH-0795-Pano-copiar.webp');" class="img-post"></div>
-            <div class="content-post">
-                <h4>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h4>
-                <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Suscipit nemo error eaque non, amet voluptate aut incidunt doloremque praesentium.
-                </p>
-                <a class="link-leiamais" href="">
-                    leia mais
-                </a>
-            </div>
-        </div>
-    </article>
+</div>
 
-
-</main>
 <?php
 get_footer();
 ?>
